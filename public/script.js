ee907ade6e33ae2449c9a1cb5e07e5dd12d16f23
SIVA SAIKUMAR REDDY K <sivasai@iMac.local>
2020-07-30 14:29:03 +0530

    basic level completed...!

---
+++ public/script.js
@@ -0,0 +1,64 @@
+const socket = io("/");
+// Getting the reference to the videoGrid.
+const videoGrid = document.getElementById("video-grid");
+//Here we are assigning the peer server port.
+const myPeer = new Peer(undefined, {
+  host: "/",
+  port: "3001",
+});
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    addVideoStream(myVideo, stream);
+
+    // We are able to receive calls using this function
+    myPeer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    // Here we are telling that when the new user joins this room show the existing participants video.
+    socket.on("user-connected", (userId) => {
+      connectToNewUser(userId, stream);
+    });
+  });
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+// After assigning the peer with get us a unique ID for each User. so we are getting that id going to use it.
+myPeer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+// We are able to make calls using this function
+function connectToNewUser(userId, stream) {
+  const call = myPeer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+  peer[userId] = call;
+}
+
+// function declaration for that stream string.
+function addVideoStream(video, stream) {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+}
---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { v4: uuidV4 } = require("uuid");
+// setting up owe express server, we are rendering owr views from 'ejs'
+app.set("view engine", "ejs");
+/* now we are going to setup our static folder,
+So now inside public folder im going to put all our Javascript and Css.
+*/
+app.use(express.static("public"));
+// Here we are going to get route here.
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+/* Here we are going to create a room and redirect the user to that room */
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.to(roomId).broadcast.emit("user-connected", userId);
+    socket.on("disconnect", () => {
+      socket.to(roomId).broadcast.emit("user-disconnected", userId);
+    });
+  });
+});
+server.listen(3000);

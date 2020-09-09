601e21066ea82dfcf2b91e90b1eab983ad2d3577
Siva saikumar reddy k <49616092+sivasai2207@users.noreply.github.com>
2020-09-10 01:36:53 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -64,7 +64,7 @@ function addVideoStream(video, stream) {
   });
   videoGrid.append(video);
 }
-const PORT = process.env.PORT || 8001;
+// const PORT = process.env.PORT || 8001;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 

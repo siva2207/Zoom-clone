64d7dc4416550adda43723f5fbad968526bda589
sivasai2207 <sivasai.reddy0@gmail.com>
2020-09-10 00:47:51 +0530

    ports upgrade..

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ io.on("connection", (socket) => {
   });
 });
 // Declaring the custom server PORT number
-const PORT =process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
